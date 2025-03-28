5388111e8711d51369ccb793b7780fa66368bd78
unknown <johnogweno64@gmail.com>
2025-03-28 11:12:28 +0300

    second  commit

---
+++ color-picker-app/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import ColorPicker from './ColorPicker.jsx'
+
+function App() {
+
+  return (
+    <>
+     <ColorPicker/>     
+    </>
+  )
+}
+
+export default App
